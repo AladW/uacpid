a4fba82358d9ec4332264acc58a33b6ba3a02ae8
Alad Wenter <alad@archlinux.org>
2020-03-28 20:58:05 +0100

    formatting

--- config.hh
+++ config.hh
@@ -3,58 +3,60 @@
 #endif
 
 static const multi_stringmap rules = {
-  {{"ac_adapter", "ACPI0003:00", "00000080", "00000000"},
-   {}
-  },
-  {{"ac_adapter", "ACPI0003:00", "00000080", "00000001"},
-   {}
-  },
-  {{"battery", "PNP0C0A:00", "00000080", "00000000"},
-   {}
-  },
-  {{"battery", "PNP0C0A:00", "00000080", "00000001"},
-   {}
-  },
-  {{"button/lid", "LID", "close", "close"},
-   {}
-  },
-  {{"button/lid", "LID", "open", "open"},
-   {}
-  },
-  {{"processor", "LNXCPU:03", "00000081", "00000000"},
-   {}
-  },
-  {{"video/tabletmode", "TBLT", "0000008A", "00000000"},
-   {"thinkpad-rotate", "normal"}
-  },
-  {{"video/tabletmode", "TBLT", "0000008A", "00000001"},
-   {"thinkpad-rotate", "inverted"}
-  }
+    {{"ac_adapter", "ACPI0003:00", "00000080", "00000000"},
+        {}
+    },
+    {{"ac_adapter", "ACPI0003:00", "00000080", "00000001"},
+        {}
+    },
+    {{"battery", "PNP0C0A:00", "00000080", "00000000"},
+        {}
+    },
+    {{"battery", "PNP0C0A:00", "00000080", "00000001"},
+        {}
+    },
+    {{"button/lid", "LID", "close", "close"},
+        {}
+    },
+    {{"button/lid", "LID", "open", "open"},
+        {}
+    },
+    {{"processor", "LNXCPU:03", "00000081", "00000000"},
+        {}
+    },
+    {{"video/tabletmode", "TBLT", "0000008A", "00000000"},
+        {"thinkpad-rotate", "normal"}
+    },
+    {{"video/tabletmode", "TBLT", "0000008A", "00000001"},
+        {"thinkpad-rotate", "inverted"}
+    }
 };
 
 static const stringmap rules_fallback = {
-  {"button/battery",
-   {}
-  },
-  {"button/mute",
-   {"amixer", "set", "Master", "toggle"}
-  },
-  {"button/sleep",
-   {}
-  },
-  {"button/volumedown",
-   {"amixer", "-D", "pulse", "set", "Master", "5%-"}
-  },
-  {"button/volumeup",
-   {"amixer", "-D", "pulse", "set", "Master", "5%+"}
-  },
-  {"video/brightnessup",
-   {"xbacklight", "-inc", "10"}
-  },
-  {"video/brightnessdown",
-   {"xbacklight", "-dec", "10"}
-  },
-  {"video/switchmode",
-   {}
-  }
+    {"button/battery",
+        {}
+    },
+    {"button/mute",
+        {"amixer", "set", "Master", "toggle"}
+    },
+    {"button/sleep",
+        {}
+    },
+    {"button/volumedown",
+        {"amixer", "-D", "pulse", "set", "Master", "5%-"}
+    },
+    {"button/volumeup",
+        {"amixer", "-D", "pulse", "set", "Master", "5%+"}
+    },
+    {"video/brightnessup",
+        {"xbacklight", "-inc", "10"}
+    },
+    {"video/brightnessdown",
+        {"xbacklight", "-dec", "10"}
+    },
+    {"video/switchmode",
+        {}
+    }
 };
+
+// vim: set et sw=4 sts=4 ft=cpp:
--- display.hh
+++ display.hh
@@ -3,34 +3,36 @@
 #include <X11/Xlib.h>
 
 void display_worker() {
-  // open connection to X display
-  Display* display = XOpenDisplay(nullptr);
+    // open connection to X display
+    Display* display = XOpenDisplay(nullptr);
 
-  if (display != nullptr) {
-    fprintf(stderr, "NOTICE: opened screen %d\n", DefaultScreen(display));
+    if (display != nullptr) {
+        fprintf(stderr, "NOTICE: opened screen %d\n", DefaultScreen(display));
 
-    // block display on thread
-    XLockDisplay(display);
+        // block display on thread
+        XLockDisplay(display);
 
-    // ignore protocol errors
-    XSetErrorHandler([](Display*, XErrorEvent* event) -> int {
-	fprintf(stderr, "Ignoring Xlib error: error code %d request code %d\n",
-		event->error_code, event->request_code);
-	return 0;
-      });
+        // ignore protocol errors
+        XSetErrorHandler([](Display*, XErrorEvent* event) -> int {
+                fprintf(stderr, "Ignoring Xlib error: error code %d request code %d\n",
+                        event->error_code, event->request_code);
+                return 0;
+                });
 
-    // block on X event queue
-    XEvent event{};
-    while (XNextEvent(display, &event)) {
-      continue;
+        // block on X event queue
+        XEvent event{};
+        while (XNextEvent(display, &event)) {
+            continue;
+        }
+
+        // handle last event
+        XCloseDisplay(display);
+    } else {
+        fprintf(stderr, "ERROR: could not open display\n");
+        std::exit(1);
     }
-
-    // handle last event
-    XCloseDisplay(display);
-  } else {
-    fprintf(stderr, "ERROR: could not open display\n");
-    std::exit(1);
-  }
 }
 
 #endif
+
+// vim: set et sw=4 sts=4 ft=cpp:
